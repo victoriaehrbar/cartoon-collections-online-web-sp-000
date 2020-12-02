a5ae061634e9027e1278b038d9550ca1a923192e
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-02 02:43:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarf_names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  # Your code here
+  planeteer_calls.map
 end
 
 def long_planeteer_calls# code an argument here
