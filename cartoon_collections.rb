4b36d8474e918d4f944f7b042ea5fc6845c12da7
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-02 02:46:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planteer_calls.any? 
+  planteer_calls.any? { |call| call.any? }
 end
 
 def find_the_cheese# code an argument here
