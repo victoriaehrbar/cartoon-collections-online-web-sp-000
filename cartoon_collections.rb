3221599e3641692b86a4c0d490dd990f728ec810
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-02 02:46:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + '!' }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planeteer_calls)
   # Your code here
 end
 
